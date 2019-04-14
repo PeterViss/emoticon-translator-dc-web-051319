422cc93ed5187c5340ce106203173b880fc348cb
Peter Viss <peter.c.viss@biola.edu>
2019-04-14 23:30:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ require 'yaml'
 
 def load_library(emoticon_file)
   # code goes here
-   emoticons = YAML.load_file('./lib/emoticons.yml')
+   emoticons = YAML.load_file('emoticons.yml')
 
    emoticon_lib = {'get_meaning'  => {},
                   'get_emoticon' => {} }
