31e733767e9258325cf7aa3fecaa7d09e88ec091
Peter Viss <peter.c.viss@biola.edu>
2019-04-14 23:37:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,8 @@ require 'yaml'
 emotes = YAML.load_file('../lib/emoticons.yml')
 # require modules here
 
-def load_library(library)
-emotes = YAML.load_file('./lib/emoticons.yml')  # code goes here
+def load_library
+emotes = YAML.load_file("./lib/emoticons.yml")  # code goes here
 end
 
 def get_japanese_emoticon
