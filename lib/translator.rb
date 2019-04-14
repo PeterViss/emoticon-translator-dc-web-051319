1411d09e5c1818b329bf52c309e1dc4d8ff6e836
Peter Viss <peter.c.viss@biola.edu>
2019-04-14 23:40:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,8 @@ require 'yaml'
 # require modules here
 
 def load_library(yaml_file)
-  # code goes here	  response = {"get_meaning" => {}, "get_emoticon" => {}}
+  # code goes here
+   response = {"get_meaning" => {}, "get_emoticon" => {}}
   library = YAML.load_file(yaml_file)
   library.each do |trans, emos|
     response["get_meaning"][emos[1]] = trans
@@ -15,7 +16,8 @@ end
 
 
 def get_japanese_emoticon(yaml_file, emoticon)
-  # code goes here	  library = load_library(yaml_file)
+  # code goes here
+  library = load_library(yaml_file)
   response = nil
   library["get_emoticon"].each do |english, japanese|
     if emoticon == english
@@ -31,7 +33,8 @@ end
 
 
 def get_english_meaning(yaml_file, emoticon)
-  # code goes here	  library = load_library(yaml_file)
+  # code goes here
+  library = load_library(yaml_file)
   response = nil
   library["get_meaning"].each do |emo, trans|
     if emo == emoticon
